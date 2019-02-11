366bd6662705623fc4019c9ca668e1af2f2c3335
ErinAlanaMac <47486649+ErinAlanaMac@users.noreply.github.com>
2019-02-11 16:18:52 -0500

    Making some preliminary changes

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ app = dash.Dash()
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1('Plotly Dash - the best way to visualize your data!'),
+    html.H1('Graphs are awesome for conveying information'),
     dcc.Graph(
         id='this_is_an_id',
         figure={
             'data': [
-                {'x': ['Dash', 'Powerpoint', 'Lascaux cave paintings'], 'y': [8, 2, 3], 'type': 'bar', 'name': 'Intelligence'},
-                {'x': ['Dash', 'Powerpoint', 'Lascaux cave paintings'], 'y': [7, 1, 5], 'type': 'bar', 'name': 'Beauty'},
+                {'x': ['Music', 'Podcasts', 'Audio Books'], 'y': [8, 2, 3], 'type': 'bar', 'name': 'Intelligence'},
+                {'x': ['Music', 'Podcasts', 'Audio Books'], 'y': [7, 1, 5], 'type': 'bar', 'name': 'Beauty'},
             ],
             'layout': {
-                'title': "Because friends don't let friends use Microsoft Powerpoint",
+                'title': "Much wow!",
                 'xaxis':{'title':'Choice of data visualization'},
                 'yaxis':{'title':'Approval rating by average data scientist'},
             }
